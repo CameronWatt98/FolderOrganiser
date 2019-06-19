b77555c653dcd7de84f96b5a78c896363ddcfcdb
Cameron Watt <camwatt3@googlemail.com>
2019-06-19 14:40:01 +0100

    Local Code Added To Master Repo

---
+++ FolderOrganiser/FolderOrganiser/FolderOrganiserForm.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace FolderOrganiser
+{
+    partial class FolderOrganiserForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label7 = new System.Windows.Forms.Label();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(139, 130);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Organise";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(139, 220);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(216, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-5, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(307, 29);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Folder Organisation Tool";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.label3.Location = new System.Drawing.Point(12, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(430, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "This folder organisation tool will arrange a folder into subfolders by date";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.label4.Location = new System.Drawing.Point(14, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(409, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "e.g. if you select \"Month\" it will move the files into monthly subfolders.";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(17, 104);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(338, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 88);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(119, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Enter full path for folder:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(140, 204);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Progress- File Move";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(17, 130);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(116, 113);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Detail Level";
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(7, 90);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(44, 17);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Day";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 58);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(55, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Month";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(7, 24);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Year";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(140, 156);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(125, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Progress- Folder Creation";
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(139, 172);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(216, 23);
+            this.progressBar2.TabIndex = 13;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(220, 133);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(134, 17);
+            this.radioButton3.TabIndex = 15;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Remove Empty Folders";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // FolderOrganiserForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 255);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Name = "FolderOrganiserForm";
+            this.Text = "Folder Organisation Tool";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.RadioButton radioButton3;
+    }
+}
+
---
+++ FolderOrganiser/FolderOrganiser/FolderOrganiserForm.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FolderOrganiser
+{
+    public partial class FolderOrganiserForm : Form
+    {
+        public FolderOrganiserForm()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try 
+            {
+                //initialise variable
+                progressBar1.Value = 0;
+                progressBar2.Value = 0;
+                var path = textBox1.Text;
+                DateTime startTime = new DateTime();
+                DateTime endTime = new DateTime();
+                String[] months = { "1-January", "2-February", "3-March", "4-April", "5-May", "6-June", "7-July", "8-August", "9-September", "10-October", "11-November", "12-December" };
+                int[] monthlength = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+                //Add files to an array and sort them
+                DirectoryInfo di = new DirectoryInfo(path);
+                FileInfo[] fiArray = di.GetFiles();
+
+                //this makes sure there are files and also prevents the user from spamming the button.
+                if (fiArray.Length == 0)
+                {
+                    NoFiles NFform = new NoFiles();
+                    NFform.StartPosition = FormStartPosition.CenterParent;
+                    NFform.ShowDialog(this);
+                }
+                else
+                {
+                    Array.Sort(fiArray, (x, y) => StringComparer.OrdinalIgnoreCase.Compare(x.CreationTime, y.CreationTime));
+                    progressBar1.Maximum = fiArray.Length;
+                    //set time for last and first created file in directory
+                    endTime = File.GetCreationTime(path + "\\" + fiArray[fiArray.Length - 1].Name);
+                    startTime = File.GetCreationTime(path + "\\" + fiArray[0].Name);
+
+
+                    //if you want subfolders split by YEARS
+                    if (radioButton1.Checked)
+                    {
+                        createSubFolders("years", path, startTime, endTime, months, monthlength, true);
+                        moveFiles("years", path, fiArray, months);
+                    }
+                    //if you want subfolders split by MONTHS
+                    else if (radioButton2.Checked)
+                    {
+                        createSubFolders("years", path, startTime, endTime, months, monthlength, false);
+                        createSubFolders("months", path, startTime, endTime, months, monthlength, true);
+                        moveFiles("months", path, fiArray, months);
+                    }
+                    //if you want subfolders split by DAYS
+                    else if (radioButton4.Checked)
+                    {
+                        createSubFolders("years", path, startTime, endTime, months, monthlength, false);
+                        createSubFolders("months", path, startTime, endTime, months, monthlength, false);
+                        createSubFolders("days", path, startTime, endTime, months, monthlength, true);
+                        moveFiles("days", path, fiArray, months);
+                    }
+                    else
+                    {
+                        NoRadioButtonSelected NRBSform = new NoRadioButtonSelected();
+                        NRBSform.StartPosition = FormStartPosition.CenterParent;
+                        NRBSform.ShowDialog(this);
+                    }
+
+                    //if you want just subfolders with data within them
+                    if (radioButton3.Checked)
+                    {
+                        removeEmptyFolders(path);
+                    }
+                }
+
+
+            }
+            catch 
+            {
+                InvalidPath form = new InvalidPath();
+                form.StartPosition = FormStartPosition.CenterParent;
+                form.ShowDialog(this);
+            }
+        }
+
+
+
+        /*-------CREATES SUBFOLDERS BASED ON THE SPECIFICATION IN VARIABLE "TIME"--------*/
+        private void createSubFolders(String time, String path, DateTime startTime, DateTime endTime, String[] months, int[] monthLength, bool updateProgressBar)
+        {
+            //if all files were created on the same day
+            if (startTime.Date == endTime.Date)
+            {
+                if (time == "years")
+                {
+                    Directory.CreateDirectory(path + "\\" + startTime.Year.ToString());
+                    progressBar2.Value = progressBar2.Maximum;
+                }
+                else if (time == "months")
+                {
+                    Directory.CreateDirectory(path + "\\" + startTime.Year.ToString() + "\\" + months[startTime.Month - 1]);
+                    progressBar2.Value = progressBar2.Maximum;
+                }
+                else if (time == "days")
+                {
+                    Directory.CreateDirectory(path + "\\" + startTime.Year.ToString() + "\\" + months[startTime.Month - 1] + "\\" + startTime.Day + "-" + startTime.DayOfWeek);
+                    progressBar2.Value = progressBar2.Maximum;
+                }
+            }
+            else
+            {
+                DateTime currentTime = new DateTime();
+                currentTime = startTime;
+                TimeSpan span = new TimeSpan();
+                span = endTime.Subtract(startTime);
+                progressBar2.Maximum = span.Days;
+                //This creates all the subfolders
+                if (time == "years")
+                {
+                    while (currentTime.Year <= endTime.Year)
+                    {
+                        //create folder
+                        Directory.CreateDirectory(path + "\\" + currentTime.Year.ToString());
+                        currentTime = currentTime.AddYears(1);
+
+                        //update progress bar
+                        if (updateProgressBar)
+                        {
+                            if (progressBar2.Value + 364 < progressBar2.Maximum)
+                            {
+                                progressBar2.Value = progressBar2.Value + 364;
+                            }
+                            else
+                            {
+                                progressBar2.Value = progressBar2.Maximum;
+                            }
+                        }
+                    }
+                }
+                else if (time == "months")
+                {
+                    while ((currentTime.Month <= endTime.Month || currentTime.Year < endTime.Year))
+                    {
+                        //create folder
+                        Directory.CreateDirectory(path + "\\" + currentTime.Year.ToString() + "\\" + months[currentTime.Month - 1]);
+                        currentTime = currentTime.AddMonths(1);
+
+                        //update progress bar
+                        if (updateProgressBar)
+                        {
+                            if (progressBar2.Value + monthLength[currentTime.Month - 1] < progressBar2.Maximum)
+                            {
+                                progressBar2.Value = progressBar2.Value + monthLength[currentTime.Month - 1];
+                            }
+                            else
+                            {
+                                progressBar2.Value = progressBar2.Maximum;
+                            }
+                        }
+                    }
+                }
+                else if (time == "days")
+                {
+                    while (currentTime.Day <= endTime.Day || currentTime.Month < endTime.Month || currentTime.Year < endTime.Year)
+                    {
+                        //create folder
+                        Directory.CreateDirectory(path + "\\" + currentTime.Year.ToString() + "\\" + months[currentTime.Month - 1] + "\\" + currentTime.Day + "-" + currentTime.DayOfWeek);
+                        currentTime = currentTime.AddDays(1);
+
+                        //update progress bar
+                        if (updateProgressBar)
+                        {
+                            if (progressBar2.Value + 1 < progressBar2.Maximum)
+                            {
+                                progressBar2.Value = progressBar2.Value + 1;
+                            }
+                            else
+                            {
+                                progressBar2.Value = progressBar2.Maximum;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        /*---------MOVES FILES INTO THE APPROPRIATE SUBFOLDERS---------*/
+        private void moveFiles(String time, String path, FileInfo[] fiArray, String[] months)
+        {
+            //add the files in the root folder to their correct subfolder
+            DateTime creationTime = new DateTime();
+            foreach (FileInfo fi in fiArray)
+            {
+                creationTime = File.GetCreationTime(path + "\\" + fi.Name);
+                if (time == "years")
+                {
+                    var targetPath = path + "\\" + creationTime.Year + "\\" + fi.Name;
+                    File.Move(path + "\\" + fi.Name, targetPath);
+                    File.SetCreationTime(targetPath, creationTime);
+                    progressBar1.Value = progressBar1.Value + 1;
+                }
+                else if(time=="months")
+                {
+                    var targetPath = path + "\\" + creationTime.Year + "\\" + months[creationTime.Month - 1] + "\\" + fi.Name;
+                    File.Move(path + "\\" + fi.Name, targetPath);
+                    File.SetCreationTime(targetPath, creationTime);
+                    progressBar1.Value = progressBar1.Value + 1;
+                }
+                else if(time=="days")
+                {
+                    var targetPath = path + "\\" + creationTime.Year + "\\" + months[creationTime.Month - 1] + 
+                                     "\\" + creationTime.Day + "-" + creationTime.DayOfWeek + "\\" + fi.Name;
+                    File.Move(path + "\\" + fi.Name, targetPath);
+                    File.SetCreationTime(targetPath, creationTime);
+                    progressBar1.Value = progressBar1.Value + 1;
+                }
+            }
+        }
+
+        private static void removeEmptyFolders(string path)
+        {
+            foreach (var directory in Directory.GetDirectories(path))
+            {
+                removeEmptyFolders(directory);
+                if (Directory.GetFiles(directory).Length == 0 &&
+                    Directory.GetDirectories(directory).Length == 0)
+                {
+                    Directory.Delete(directory, false);
+                }
+            }
+        }
+
+    }
+}
